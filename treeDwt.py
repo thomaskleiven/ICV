5f38f5100a19147b22b1176b88f92e10425f2604
Thomas Kleiven <tkleiven@localhost.localdomain>
2018-03-15 19:02:26 +0100

    Added hdf5

--- treeDwt.py
+++ treeDwt.py
@@ -6,10 +6,13 @@ from os import listdir
 from os.path import isfile, join
 from numpy import linalg as LA
 from pprint import pprint
+import h5py
+
+dataset = h5py.File("dataset.hdf5", "r+")
 
 HEIGHT=512
 WIDTH=512
-MAX_LEVEL=1
+MAX_LEVEL=2
 
 class DWTRootTree():
     def __init__(self, img, maxLevel):
@@ -25,17 +28,17 @@ class DWTRootTree():
         self.C2 = np.empty(shape=(HEIGHT, WIDTH))
         self.C3 = np.empty(shape=(HEIGHT, WIDTH))
 
-    def run(self):
+    def run(self, path):
         self.getRGBvalues()
-        self.C1_cA, self.C1_cV, self.C1_cD, self.C1_cH = self.dwt2(self.C1)
-        self.C2_cA, self.C2_cV, self.C2_cD, self.C2_cH = self.dwt2(self.C2)
-        self.C3_cA, self.C3_cV, self.C3_cD, self.C3_cH = self.dwt2(self.C3)
+        self.C1_cA, self.C1_cV, self.C1_cD, self.C1_cH = self.dwt2(self.C1, path, id='C1')
+        self.C2_cA, self.C2_cV, self.C2_cD, self.C2_cH = self.dwt2(self.C2, path, id='C2')
+        self.C3_cA, self.C3_cV, self.C3_cD, self.C3_cH = self.dwt2(self.C3, path, id='C3')
 
     # DWT Algorithm
-    def dwt2(self, img, wavelet='db1'):
+    def dwt2(self, img, path,id, wavelet='db1'):
         coeffs = pywt.dwt2(img, wavelet)
         cA, (cH, cV, cD) = coeffs
-        return  DWTSubTree(cA, level=self.level, maxLevel=self.maxLevel), cH, cV, cD
+        return  DWTSubTree(cA, path, id=id, level=self.level, maxLevel=self.maxLevel), cH, cV, cD
 
     def showImage(self, image, window='window'):
         cv2.imshow('window', image)
@@ -58,37 +61,46 @@ class DWTRootTree():
 
 
 class DWTSubTree():
-    def __init__(self, img, level, maxLevel):
+    def __init__(self, img, path, level, maxLevel, id):
 
         self.cA, self.cV, self.cD, self.cH = None, None, None, None
         self.level = level + 1
         self.maxLevel = maxLevel
         self.image = img
-        self.run()
+        self.id = id
+        self.path = path
+        self.run(path)
 
-    def run(self):
-        self.cA, self.cV, self.cD, self.cH = self.dwt2(self.image)
+    def run(self, path):
+        self.cA, self.cH, self.cV, self.cD = self.dwt2(self.image, path)
 
     # DWT Algorithm
-    def dwt2(self, img, wavelet='db1'):
+    def dwt2(self, img, path, wavelet='db1',):
         coeffs = pywt.dwt2(img, wavelet)
         cA, (cH, cV, cD) = coeffs
-        if (self.level == self.maxLevel):
-            return cA, cH, cV, cD
 
-        return  DWTSubTree(cA, level=self.level, maxLevel=self.maxLevel), cV, cD, cH
+        if (self.level == self.maxLevel):
+            try:
+                dataset.create_dataset("%s/level%s/%s/cA/"%(path, self.level, self.id), data=cA, dtype=np.float)
+                dataset.create_dataset("%s/level%s/%s/cH/"%(path, self.level, self.id), data=cH, dtype=np.float)
+                dataset.create_dataset("%s/level%s/%s/cV/"%(path, self.level, self.id), data=cV, dtype=np.float)
+                dataset.create_dataset("%s/level%s/%s/cD/"%(path, self.level, self.id), data=cD, dtype=np.float)
+            except Exception as exc:
+                print(str(exc))
+            return cA, cH, cV, cD
+        return  DWTSubTree(cA, path=self.path, id=self.id, level=self.level, maxLevel=self.maxLevel), cV, cD, cH
 
 
 img_path = [arg for arg in sys.argv]
 images_path = [f for f in listdir('./images/dataset') if isfile(join('./images/dataset', f))]
 
-if __name__ == "__main__":
+def main():
     result = []
     img = np.uint8(cv2.imread(img_path[1]))
     img = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
 
     QueryImageClass = DWTRootTree(img, maxLevel=MAX_LEVEL)
-    QueryImageClass.run()
+    QueryImageClass.run(images_path[1])
 
     def getLeafs(tree):
         if (tree.maxLevel == tree.level):
@@ -100,18 +112,38 @@ if __name__ == "__main__":
     C3_cA_q, C3_cH_q, C3_cV_q, C3_cD_q = getLeafs(QueryImageClass.C3_cA)
 
     for path in images_path:
-        dataset_img = np.uint8(cv2.imread('./images/dataset/%s'%path))
-        dataset_img = cv2.cvtColor(dataset_img, cv2.COLOR_BGR2HSV)
-        DatasetImageClass = DWTRootTree(dataset_img, maxLevel=MAX_LEVEL)
-        DatasetImageClass.run()
-        C1_cA_d, C1_cH_d, C1_cV_d, C1_cD_d = getLeafs(DatasetImageClass.C1_cA)
-        C2_cA_d, C2_cH_d, C2_cV_d, C2_cD_d = getLeafs(DatasetImageClass.C2_cA)
-        C3_cA_d, C3_cH_d, C3_cV_d, C3_cD_d = getLeafs(DatasetImageClass.C3_cA)
+        if ("%s/level%s"%(path, MAX_LEVEL) in dataset):
+            C1_cA_d, C1_cH_d, C1_cV_d, C1_cD_d = dataset[path]['level%s'%MAX_LEVEL]['C1']['cA'].value,\
+                                                 dataset[path]['level%s'%MAX_LEVEL]['C1']['cH'].value,\
+                                                 dataset[path]['level%s'%MAX_LEVEL]['C1']['cV'].value,\
+                                                 dataset[path]['level%s'%MAX_LEVEL]['C1']['cD'].value
+
+            C2_cA_d, C2_cH_d, C2_cV_d, C2_cD_d = dataset[path]['level%s'%MAX_LEVEL]['C2']['cA'].value,\
+                                                 dataset[path]['level%s'%MAX_LEVEL]['C2']['cH'].value,\
+                                                 dataset[path]['level%s'%MAX_LEVEL]['C2']['cV'].value,\
+                                                 dataset[path]['level%s'%MAX_LEVEL]['C2']['cD'].value
+
+            C3_cA_d, C3_cH_d, C3_cV_d, C3_cD_d = dataset[path]['level%s'%MAX_LEVEL]['C3']['cA'].value,\
+                                                 dataset[path]['level%s'%MAX_LEVEL]['C3']['cH'].value,\
+                                                 dataset[path]['level%s'%MAX_LEVEL]['C3']['cV'].value,\
+                                                 dataset[path]['level%s'%MAX_LEVEL]['C3']['cD'].value
+
+        else:
+            dataset_img = np.uint8(cv2.imread('./images/dataset/%s'%path))
+            dataset_img = cv2.cvtColor(dataset_img, cv2.COLOR_BGR2HSV)
+            DatasetImageClass = DWTRootTree(dataset_img, maxLevel=MAX_LEVEL)
+            DatasetImageClass.run(path)
+            C1_cA_d, C1_cH_d, C1_cV_d, C1_cD_d = getLeafs(DatasetImageClass.C1_cA)
+            C2_cA_d, C2_cH_d, C2_cV_d, C2_cD_d = getLeafs(DatasetImageClass.C2_cA)
+            C3_cA_d, C3_cH_d, C3_cV_d, C3_cD_d = getLeafs(DatasetImageClass.C3_cA)
 
         distance =  LA.norm(C1_cA_q - C1_cA_d, ord=2) + LA.norm(C1_cH_q - C1_cH_d, ord=2) + LA.norm(C1_cV_q - C1_cV_d, ord=2) + LA.norm(C1_cD_q - C1_cD_d, ord=2)+\
                     LA.norm(C2_cA_q - C2_cA_d, ord=2) + LA.norm(C2_cH_q - C2_cH_d, ord=2) + LA.norm(C2_cV_q - C2_cV_d, ord=2) + LA.norm(C2_cD_q - C2_cD_d, ord=2)+\
                     LA.norm(C3_cA_q - C3_cA_d, ord=2) + LA.norm(C3_cH_q - C3_cH_d, ord=2) + LA.norm(C3_cV_q - C3_cV_d, ord=2) + LA.norm(C3_cD_q - C3_cD_d, ord=2)
 
         result.append({ 'path': path, 'distance': distance})
+    [print('Image %s, distance: %s'%(pic['path'], pic['distance'])) for pic in sorted(result, key=lambda k: k['distance'])[0:15]]
 
-[print(pic) for pic in sorted(result, key=lambda k: k['distance'])[0:15]]
+if __name__ == "__main__":
+    main()
+    pass
