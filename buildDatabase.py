da1858b279ca786b7e7a8582e2121e7164573c44
Thomas Kleiven <tkleiven@localhost.localdomain>
2018-03-19 08:36:51 +0100

    Added KD-tree for PCA

--- buildDatabase.py
+++ buildDatabase.py
@@ -7,9 +7,10 @@ import h5py
 from sklearn import decomposition
 from multiprocessing import Pool
 import sys
+import _pickle as cPickle
 
-images_path = [f for f in listdir('./images/dataset') if isfile(join('./images/dataset', f))]
 args = [arg for arg in sys.argv]
+images_path = [f for f in listdir(args[1]) if isfile(join(args[1], f))]
 
 HEIGHT=64
 WIDTH=64
@@ -34,9 +35,9 @@ class DWTRootTree():
         self.getRGBvalues()
 
         if ('--pca' in args):
-            self.components_C1 = self.pca.fit_transform(self.C1)[0:20]
-            self.components_C2 = self.pca.fit_transform(self.C2)[0:20]
-            self.components_C3 = self.pca.fit_transform(self.C3)[0:20]
+            self.components_C1 = self.pca.fit_transform(self.C1)[0:10]
+            self.components_C2 = self.pca.fit_transform(self.C2)[0:10]
+            self.components_C3 = self.pca.fit_transform(self.C3)[0:10]
 
             self.C1_cA, self.C1_cV, self.C1_cD, self.C1_cH = self.dwt2(self.components_C1)
             self.C2_cA, self.C2_cV, self.C2_cD, self.C2_cH = self.dwt2(self.components_C2)
@@ -46,6 +47,11 @@ class DWTRootTree():
             self.C1_cA, self.C1_cV, self.C1_cD, self.C1_cH = self.dwt2(self.C1)
             self.C2_cA, self.C2_cV, self.C2_cD, self.C2_cH = self.dwt2(self.C2)
             self.C3_cA, self.C3_cV, self.C3_cD, self.C3_cH = self.dwt2(self.C3)
+
+    def showImage(self, image, window='window'):
+        cv2.imshow('window', image)
+        cv2.waitKey()
+
 
     # DWT Algorithm
     def dwt2(self, img, wavelet='db1'):
@@ -60,18 +66,11 @@ class DWTRootTree():
         return resized
 
     def getRGBvalues(self):
-        # Iterate all pixels
-        for row in range(0, HEIGHT):
-            for col in range(0, WIDTH):
-                C1_pixel, C2_pixel, C3_pixel = self.image[row, col, 0], self.image[row, col, 1], self.image[row, col, 2]
-                self.C1[row][col] = C1_pixel
-                self.C2[row][col] = C2_pixel
-                self.C3[row][col] = C3_pixel
+        self.C1, self.C2, self.C3 = cv2.split(self.image)
 
 
 class DWTSubTree():
     def __init__(self, img, level, maxLevel):
-
         self.cA, self.cV, self.cD, self.cH = None, None, None, None
         self.level = level + 1
         self.maxLevel = maxLevel
@@ -97,8 +96,8 @@ def getLeafNodes(tree):
     return getLeafNodes(tree.cA)
 
 def main(path):
-    dataset_img = np.uint8(cv2.imread('./images/dataset/%s'%path))
-    dataset_img = cv2.cvtColor(dataset_img, cv2.COLOR_BGR2HSV)
+    dataset_img = np.uint8(cv2.imread(args[1]+'/%s'%path))
+    dataset_img = cv2.cvtColor(dataset_img, cv2.COLOR_BGR2HSV) / 255
     DatasetImageClass = DWTRootTree(dataset_img, maxLevel=MAX_LEVEL)
     DatasetImageClass.run()
 
@@ -106,39 +105,23 @@ def main(path):
     C2_cA, C2_cH, C2_cV, C2_cD = getLeafNodes(DatasetImageClass.C2_cA)
     C3_cA, C3_cH, C3_cV, C3_cD = getLeafNodes(DatasetImageClass.C3_cA)
 
-    return {
-     'C1_cA' if '--pca' not in args else 'pca/C1_cA': C1_cA,\
-     'C1_cH' if '--pca' not in args else 'pca/C1_cH': C1_cH,\
-     'C1_cV' if '--pca' not in args else 'pca/C1_cV': C1_cV,\
-     'C1_cD' if '--pca' not in args else 'pca/C1_cD': C1_cD,\
-     'C2_cA' if '--pca' not in args else 'pca/C2_cA': C2_cA,\
-     'C2_cH' if '--pca' not in args else 'pca/C2_cH': C2_cH,\
-     'C2_cV' if '--pca' not in args else 'pca/C2_cV': C2_cV,\
-     'C2_cD' if '--pca' not in args else 'pca/C2_cD': C2_cD,\
-     'C3_cA' if '--pca' not in args else 'pca/C3_cA': C3_cA,\
-     'C3_cH' if '--pca' not in args else 'pca/C3_cH': C3_cH,\
-     'C3_cV' if '--pca' not in args else 'pca/C3_cV': C3_cV,\
-     'C3_cD' if '--pca' not in args else 'pca/C3_cD': C3_cD,\
-     'path': path }
-
-
-def handle_output(result):
-    dataset = h5py.File('dataset.hdf5', 'a')
-    for key, value in result.items():
-        if (key == 'path'):
-            continue
-        try:
-            grp = dataset.create_dataset("%s/level%s/%s"%(result['path'], MAX_LEVEL, key), data=value, dtype=np.float)
-        except Exception as exc:
-            pass
-            #print(str(exc))
+    return list(zip(np.ravel(C1_cA), np.ravel(C1_cH), np.ravel(C1_cV),\
+                    np.ravel(C1_cD), np.ravel(C2_cA), np.ravel(C2_cH),\
+                    np.ravel(C2_cV), np.ravel(C2_cD), np.ravel(C3_cA),\
+                    np.ravel(C3_cH), np.ravel(C3_cV),  np.ravel(C3_cD)))
 
 if __name__ == "__main__":
     pool=Pool()
-    dataset = h5py.File('dataset.hdf5', 'a')
+    m = []
     result = []
     for path in images_path:
-        r = pool.apply_async(main, (path,), callback=handle_output)
+        r = pool.apply_async(main, (path,))
         result.append(r)
     for r in result:
-        r.wait()
+        m.append(r.get())
+
+    t = [item for sublist in m for item in sublist]
+    from sklearn.neighbors import KDTree
+    tree = KDTree(t)
+
+    cPickle.dump(tree, open('tree_pca.p' if'--pca' in args else 'tree.p','wb'))
